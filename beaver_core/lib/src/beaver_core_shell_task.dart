c9b5afb06c46d88ec39d20cda73f998605a7962a
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-07-26 15:55:44 +0900

    Implement ShellTask

--- beaver_core/lib/src/beaver_core_shell_task.dart
+++ beaver_core/lib/src/beaver_core_shell_task.dart
@@ -2,8 +2,20 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:io';
 
 import './beaver_core_base.dart';
+
+class ShellError extends TaskError {
+  final String _executable;
+
+  final int _exitCode;
+
+  ShellError(this._executable, this._exitCode);
+
+  @override
+  String toString() => '${_executable} exited with ${_exitCode}';
+}
 
 class ShellTask extends Task {
   @override
@@ -17,7 +29,18 @@ class ShellTask extends Task {
 
   @override
   Future<Object> execute(Context context) async {
-    throw new UnimplementedError();
+    // FIXME: Process.run may throw a ProcessException if executable does not exist.
+    final result = await Process.run(executable, arguments);
+    final infoMessage = result.stdout.toString();
+    if (infoMessage.isNotEmpty) {
+      context.logger.info(infoMessage);
+    }
+    final errorMessage = result.stderr.toString();
+    if (errorMessage.isNotEmpty) {
+      context.logger.error(errorMessage);
+    }
+    if (result.exitCode != 0) {
+      throw new ShellError(executable, result.exitCode);
+    }
   }
 }
-
