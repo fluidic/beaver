d60f2cb6454c16508691793617452b54b2d21248
Kyusun Kim <maniagoon@gmail.com>
2016-07-27 18:53:50 +0900

    Implement GCloudStorageUploadTask
    storage.createBucket has the problem that can not create a bucket. I
    don't know why yet. So I added FIXME there.

--- beaver_core/lib/src/beaver_core_gcloud_storage_upload_task.dart
+++ beaver_core/lib/src/beaver_core_gcloud_storage_upload_task.dart
@@ -2,8 +2,19 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:io';
 
 import './beaver_core_base.dart';
+import './beaver_core_gcloud_context.dart';
+
+class GCloudStorageUploadException extends TaskException {
+  final _message;
+
+  GCloudStorageUploadException(this._message);
+
+  @override
+  String toString() => _message;
+}
 
 /// Upload a file to Google Cloud Storage.
 class GCloudStorageUploadTask extends Task {
@@ -18,8 +29,37 @@ class GCloudStorageUploadTask extends Task {
 
   GCloudStorageUploadTask(this.src, this.bucketName);
 
+  /// Return a download link for the uploaded file.
   @override
-  Future<String> execute(Context context) async {
-    throw new UnimplementedError();
+  Future<Uri> execute(Context context) async {
+    final file = new File(src);
+    if (!await file.exists()) {
+      throw new GCloudStorageUploadException(
+          'Source file \'${src}\' does not exist.');
+    }
+
+    if (!(context is GCloudContext)) {
+      throw new GCloudStorageUploadException('context is not GCloudContext.');
+    }
+    final storage = (context as GCloudContext).storage;
+
+    var bucket;
+    if (!await storage.bucketExists(bucketName)) {
+      // FIXME: This line fails with the following error.
+      //     DetailedApiRequestError(status: 403, message: The account for
+      //     the specified project has been disabled.)
+      bucket = await storage.createBucket(bucketName);
+    } else {
+      bucket = await storage.bucket(bucketName);
+    }
+
+    final objectName = _getSuggestedObjectName(src);
+    await file.openRead().pipe(bucket.write(objectName));
+
+    final objectInfo = await bucket.info(objectName);
+    return objectInfo.downloadLink;
   }
+
+  static String _getSuggestedObjectName(String filePath) =>
+      Uri.parse(filePath).pathSegments.last;
 }
