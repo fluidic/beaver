c071dd25c580684798cef32abaaecacbbeaac0b4
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-07-27 11:19:32 +0900

    Change the copyright to Fluidic Inc

--- beaver_core/example/beaver_core_example.dart
+++ beaver_core/example/beaver_core_example.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2016, Kwang Yul Seo. All rights reserved. Use of this source code
+// Copyright (c) 2016, Fluidic Inc. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
