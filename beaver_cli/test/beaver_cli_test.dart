3c1dedcc5acba362f68dcf8e6f56748960a72b9b
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-07-27 16:24:20 +0900

    Initial import of beaver_cli

---
+++ beaver_cli/test/beaver_cli_test.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2016, Kwang Yul Seo. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:beaver_cli/beaver_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
