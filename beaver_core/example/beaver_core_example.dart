7c2fc840ef03be19caaffacb1b630200a9f363c7
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-07-29 12:11:50 +0900

    Make runBeaver accept ExecuteFunc, Task and Iterable<Task>

--- beaver_core/example/beaver_core_example.dart
+++ beaver_core/example/beaver_core_example.dart
@@ -1,16 +1,12 @@
 // Copyright (c) 2016, Fluidic Inc. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
 import 'package:beaver_core/beaver_core.dart';
 import 'package:beaver_dart_task/beaver_dart_task.dart';
 
-main() => runBeaver((Context context) async {
-      List<Task> tasks = [
-        new InstallDartSdkTask(withContentShell: true, withDartium: true),
-        new GitTask(['clone', 'git@github.com:fluidic/symbol.git']),
-        new PubTask(['get'], processWorkingDir: 'symbol'),
-        new PubTask(['run', 'test'], processWorkingDir: 'symbol')
-      ];
-      return Future.forEach(tasks, (task) => task.execute(context));
-    });
+main() => runBeaver([
+      new InstallDartSdkTask(withContentShell: true, withDartium: true),
+      new GitTask(['clone', 'git@github.com:fluidic/symbol.git']),
+      new PubTask(['get'], processWorkingDir: 'symbol'),
+      new PubTask(['run', 'test'], processWorkingDir: 'symbol')
+    ]);
--- beaver_core/lib/src/beaver_core_run.dart
+++ beaver_core/lib/src/beaver_core_run.dart
@@ -11,17 +11,27 @@ import './beaver_core_logger.dart';
 import './beaver_core_reporter.dart';
 import './beaver_core_task_runner.dart';
 
-Future runBeaver(ExecuteFunc func) async {
+Future runBeaver(obj) async {
   Configuration conf = new YamlConfiguration.fromFile('beaver.yaml');
   // FIXME: Don't hardcode parts and logger.
   final parts = [new GCloudContextPart()];
   final logger = new ConsoleLogger();
   Context context =
       await DefaultContext.create(conf, parts: parts, logger: logger);
-  TaskRunner runner = new TaskRunner(context, func);
+
+  var task;
+  if (obj is Function || obj is Task) {
+    task = obj;
+  } else if (obj is Iterable<Task>) {
+    task = (Context context) => Future.forEach(obj, (t) => t.execute(context));
+  } else {
+    throw new ArgumentError(
+        'argument must be either ExecuteFunc, Task or Iterable<Task>');
+  }
+
+  TaskRunner runner = new TaskRunner(context, task);
   TaskRunResult result = await runner.run();
   // FIXME: Don't hardcode reporter.
   JsonReporter reporter = new JsonReporter(result);
   print(reporter.toJson());
 }
-
