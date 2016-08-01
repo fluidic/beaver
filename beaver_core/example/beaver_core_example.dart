e442371213aa3d8e53ab4db833a1f1d83be4f12c
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-08-01 12:42:24 +0900

    Add repository key to beaver.yaml
    Change TaskRunenr to clone the repo before running the task.

--- beaver_core/example/beaver_core_example.dart
+++ beaver_core/example/beaver_core_example.dart
@@ -1,11 +1,8 @@
 import 'package:beaver_core/beaver_core.dart';
 import 'package:beaver_dart_task/beaver_dart_task.dart';
 
-main() => runBeaver(seq([
-      par([
-        new InstallDartSdkTask(withContentShell: true, withDartium: true),
-        new GitTask(['clone', 'git@github.com:fluidic/symbol.git'])
-      ]),
+main() => runBeaver([
+      new InstallDartSdkTask(withContentShell: true, withDartium: true),
       new PubTask(['get'], processWorkingDir: 'symbol'),
       new PubTask(['run', 'test'], processWorkingDir: 'symbol')
-    ]));
+    ]);
--- beaver_core/lib/src/task_runner.dart
+++ beaver_core/lib/src/task_runner.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import './base.dart';
+import './task/git_task.dart';
 
 class TaskRunner {
   final Context context;
@@ -9,7 +10,15 @@ class TaskRunner {
   TaskRunner(this.context, /* Task|ExecuteFunc */ task)
       : this.task = task is Task ? task : new Task.fromFunc(task);
 
+  Future<Null> _cloneRepo() async {
+    // FIXME: Support the repository types other than git.
+    final repo = context.configuration['repository']['location'];
+    await new GitTask(['clone', repo]).execute(context);
+  }
+
   Future<TaskRunResult> run() async {
+    await _cloneRepo();
+
     var status = TaskStatus.Success;
     final logger = context.logger;
     try {
