0060fb8c97109702fab1e7a786dead596fcbdf18
Kyusun Kim <maniagoon@gmail.com>
2016-08-02 16:14:24 +0900

    Use file_helper in MkdirTask
    Also TaskClass annotation is added and obsolete name getter is removed.

--- beaver_core/lib/src/task/mkdir_task.dart
+++ beaver_core/lib/src/task/mkdir_task.dart
@@ -1,23 +1,20 @@
 import 'dart:async';
-import 'dart:io';
 
+import 'package:file_helper/file_helper.dart';
+
+import '../annotation.dart';
 import '../base.dart';
 
 /// Creates a directory. Also non-existent parent directories are created,
 /// when necessary. Does nothing if the directory already exist.
+@TaskClass('mkdir')
 class MkdirTask extends Task {
-  @override
-  String get name => "mkdir";
-
-  /// The directory to create.
+  /// The directory will be created.
   final String dir;
 
   MkdirTask(this.dir);
 
   @override
-  Future<Directory> execute(Context context) async {
-    final directory = new Directory(dir);
-    await directory.create(recursive: true);
-    return directory;
-  }
+  Future<Object> execute(Context context) =>
+      FileHelper.mkdir([dir], recursive: true);
 }
