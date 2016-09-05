534c60ea22c356ba4f6195201b9726533b8cdb8b
Kyusun Kim <maniagoon@gmail.com>
2016-09-05 16:25:42 +0900

    [config_store] Remove obsolete FIXME

--- beaver_config_store/lib/src/model/project.dart
+++ beaver_config_store/lib/src/model/project.dart
@@ -1,7 +1,5 @@
 import 'package:beaver_task/beaver_task.dart';
 
-// FIXME: This model should have the key-value configuration data that made from
-// configFile.
 class Project {
   final String name;
   String id;
