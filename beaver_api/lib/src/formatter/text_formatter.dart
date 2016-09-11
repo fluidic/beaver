9e600f8e737ea7031073ec6bbd1c433a6b8965ba
JinGyeong Jeong <jjgjoojis@gmail.com>
2016-09-11 19:45:24 +0900

    [api] Implement TextFormatter.

--- beaver_api/lib/src/formatter/text_formatter.dart
+++ beaver_api/lib/src/formatter/text_formatter.dart
@@ -1,3 +1,4 @@
+import 'package:beaver_task/src/run.dart';
 import 'package:beaver_trigger_handler/beaver_trigger_handler.dart';
 
 import '../formatter.dart';
@@ -10,5 +11,30 @@ class TextFormatter implements Formatter {
 
   TextFormatter(this._result);
 
-  String toText() => _result.toString();
+  String toText() {
+    final buffer = new StringBuffer();
+
+    buffer.writeln("${_result.project.name} (${_result.project.id})");
+
+    final items = {
+      "Build Number": _result.project.buildNumber.toString(),
+      "TaskInstance Status": _result.taskInstanceRunResult.status ==
+          TaskInstanceStatus.success ? "Success" : "Failure",
+      "Task Status": _result.taskInstanceRunResult.taskRunResult.status ==
+          TaskStatus.Success ? "Success" : "Failure",
+      "Trigger Event": _result.parsedTrigger.event,
+      "Trigger URL": _result.parsedTrigger.url,
+      "Log": _result.taskInstanceRunResult.taskRunResult.log,
+    };
+
+    final longestKeyLength = items.keys.fold(0, (num max, String keyString) {
+      return keyString.length > max ? keyString.length : max;
+    });
+    items.forEach((String itemName, String itemContent) {
+      buffer.writeln(
+          " - ${itemName.padRight(longestKeyLength + 1)}: ${itemContent}");
+    });
+
+    return buffer.toString();
+  }
 }
