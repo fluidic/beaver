b65d5284b5b38d25cd4ca5c1d0a22b6ed78310f0
Kyusun Kim <maniagoon@gmail.com>
2016-09-29 17:28:32 +0900

    [frontend_gcf] Enhance make_directory_for_upload.dart

--- beaver_frontend_gcf/tool/make_directory_for_upload.dart
+++ beaver_frontend_gcf/tool/make_directory_for_upload.dart
@@ -8,17 +8,36 @@ const executableDartSnapshotFile = '${executableDartFile}.snapshot';
 
 /// This tool should be run beaver_frontend_gcf directory.
 main() async {
-  mkdir([uploadDirectory]);
+  await rm([uploadDirectory], directory: true, recursive: true);
 
-  await Process.run('dart', [
-    '--snapshot=${executableDartFile}.snapshot',
+  if (!await mkdir([uploadDirectory])) {
+    print('Cannot create ${uploadDirectory} directory.');
+    return;
+  }
+
+  final r = await Process.run('dart', [
+    '--snapshot=${executableDartSnapshotFile}',
     'bin/${executableDartFile}'
   ]);
+  if (r.exitCode != 0) {
+    print('Cannot create ${executableDartSnapshotFile}.');
+    print(r.stdout);
+    print(r.stderr);
+    return;
+  }
 
-  move([executableDartSnapshotFile], uploadDirectory);
+  if (!await move([executableDartSnapshotFile], uploadDirectory)) {
+    print('${executableDartSnapshotFile} cannot be moved.');
+    return;
+  }
 
-  copy([
+  if (!await copy([
     'third_party',
     'function/index.js',
-  ], uploadDirectory);
+  ], uploadDirectory)) {
+    print('third_party or function/index.js cannot be copied.');
+    return;
+  }
+
+  print('Done.');
 }
