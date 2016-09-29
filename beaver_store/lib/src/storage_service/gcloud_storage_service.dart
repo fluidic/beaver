be12e1b18d01c4c5896622df2f4b0b77d68b8863
JinGyeong Jeong <jjgjoojis@gmail.com>
2016-09-30 03:51:12 +0900

    [store] Implement save|loadProject for GCloudStorageService.

--- beaver_store/lib/src/storage_service/gcloud_storage_service.dart
+++ beaver_store/lib/src/storage_service/gcloud_storage_service.dart
@@ -1,22 +1,32 @@
 import 'dart:async';
 
 import 'package:beaver_gcloud/src/gcloud_mixin.dart';
+import 'package:beaver_store/src/model/config.dart';
 import 'package:beaver_trigger_handler/beaver_trigger_handler.dart';
+import 'package:gcloud/datastore.dart' as datastore;
 
 import '../model/project.dart';
 import '../storage_service.dart';
+import 'gcloud_model/project.dart';
 
 class GCloudStorageService extends Object
     with GCloudMixin
     implements StorageService {
-  @override
-  Future<String> loadConfigFile(String projectId) {
-    // TODO: implement loadConfigFile
+  Future<String> _allocateProjectId() async {
+    final keyElement = new datastore.KeyElement('BeaverProject', null);
+    final key = new datastore.Key([keyElement]);
+    final populatedKey = (await db.datastore.allocateIds([key])).first;
+    return populatedKey.elements.first.id.toString();
+  }
+
+  Future<BeaverProject> _queryProjectModel(String projectId) async {
+    final query = db.query(BeaverProject)..filter('projectId =', projectId);
+    return query.run().first;
   }
 
   @override
-  Future<Project> loadProject(String projectId) {
-    // TODO: implement loadProject
+  Future<String> loadConfigFile(String projectId) {
+    // TODO: implement loadConfigFile
   }
 
   @override
@@ -25,8 +35,25 @@ class GCloudStorageService extends Object
   }
 
   @override
-  Future<String> saveProject(Project project) {
-    // TODO: implement saveProject
+  Future<Project> loadProject(String projectId) async {
+    final projectModel = await _queryProjectModel(projectId);
+    return new Project(projectModel.name)
+      ..id = projectModel.projectId
+      ..config = new YamlConfig(projectModel.config);
+  }
+
+  @override
+  Future<String> saveProject(Project project) async {
+    final projectModel = project.id == null
+        ? (new BeaverProject()
+          ..projectId = await _allocateProjectId()
+          ..buildNumber = 0)
+        : await _queryProjectModel(project.id);
+    projectModel
+      ..name = project.name
+      ..config = project.config.toString();
+    await db.commit(inserts: [projectModel]);
+    return projectModel.projectId;
   }
 
   @override
