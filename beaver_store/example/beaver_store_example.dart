c14491d7cd7ec6f259ed7db78741365c67fc23bd
Kyusun Kim <maniagoon@gmail.com>
2016-10-17 17:05:13 +0900

    [store] Remove configFile accessing

--- beaver_store/example/beaver_store_example.dart
+++ beaver_store/example/beaver_store_example.dart
@@ -14,7 +14,6 @@ main() async {
   final project = await store.getProject(id);
   print(project.config['project']);
   print(project.config['description']);
-  print(project.configFile);
 }
 
 String _loadYamlFile(String path) {
