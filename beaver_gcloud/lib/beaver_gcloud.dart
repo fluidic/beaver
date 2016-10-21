ce4cf3dc7144081560ed0564ea488d27cac60518
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-10-21 14:12:39 +0900

    Export sshKeyPath

--- beaver_gcloud/lib/beaver_gcloud.dart
+++ beaver_gcloud/lib/beaver_gcloud.dart
@@ -1,3 +1,4 @@
 library beaver_gcloud;
 
 export 'src/gcloud_mixin.dart';
+export 'src/ssh_keygen.dart' show sshKeyPath;
--- beaver_gcloud/lib/src/ssh_keygen.dart
+++ beaver_gcloud/lib/src/ssh_keygen.dart
@@ -12,12 +12,12 @@ final String _homePath = Platform.isWindows
 final CommandWrapper _sshKeygen = new CommandWrapper('ssh-keygen');
 
 final String _beaverDir = path.join(_homePath, '.beaver');
-final String _sshKeyPath = path.join(_beaverDir, 'id_rsa');
 
-final String sshPublicKeyPath = '${_sshKeyPath}.pub';
+final String sshKeyPath = path.join(_beaverDir, 'id_rsa');
+final String sshPublicKeyPath = '${sshKeyPath}.pub';
 
 Future<Null> generateSshKeyIfNotExist() async {
-  if (await new File(_sshKeyPath).exists()) return;
+  if (await new File(sshKeyPath).exists()) return;
 
   if (!await new File(_beaverDir).exists()) {
     await mkdir([_beaverDir], recursive: true);
@@ -25,7 +25,7 @@ Future<Null> generateSshKeyIfNotExist() async {
 
   const username = 'beaver';
   CommandResult result =
-      await _sshKeygen.run(['-t', 'rsa', '-f', _sshKeyPath, '-C', username]);
+      await _sshKeygen.run(['-t', 'rsa', '-f', sshKeyPath, '-C', username]);
   if (result.exitCode != 0) {
     throw new Exception('Fail to create ssh key');
   }
