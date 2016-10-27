be3c70c5e3e742643153b2db09843e4785f34c68
Kyusun Kim <maniagoon@gmail.com>
2016-10-27 14:44:13 +0900

    [task] Add MailTask

--- beaver_task/lib/beaver_task.dart
+++ beaver_task/lib/beaver_task.dart
@@ -2,13 +2,14 @@ library beaver_task;
 
 export 'src/annotation.dart';
 export 'src/base.dart';
-export 'src/gcloud_context.dart';
 export 'src/exception.dart';
+export 'src/gcloud_context.dart';
 export 'src/task.dart';
 export 'src/task/copy_task.dart';
 export 'src/task/delete_task.dart';
 export 'src/task/download_task.dart';
 export 'src/task/git_task.dart';
+export 'src/task/mail_task.dart';
 export 'src/task/mkdir_task.dart';
 export 'src/task/move_task.dart';
 export 'src/task/post_task.dart';
---
+++ beaver_task/lib/src/task/mail_task.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:beaver_gcloud_task/beaver_gcloud_task.dart';
+
+import '../annotation.dart';
+import '../base.dart';
+import '../task.dart';
+
+@TaskClass('mail')
+class MailTask extends Task {
+  /// Receiver mail address.
+  final String to;
+
+  MailTask(this.to);
+
+  MailTask.fromArgs(List<String> args) : this(args[0]);
+
+  @override
+  Future<Object> execute(Context context) {
+    final subject = 'Beaver CI Mail Notification';
+    // FIXME: Pass trigger_handler's info like build number.
+    final content = '''
+    Task is triggerred.
+    Task Log: ${context.logger.toString()}
+    ''';
+
+    var task;
+    switch (context.config.cloudType) {
+      case 'gcloud':
+        task = new GCloudMailTask(to, subject, content);
+        break;
+      default:
+        // FIXME: Support the other cloud platforms.
+        throw new TaskException('Not supported cloud type.');
+    }
+
+    return task.execute(context);
+  }
+}
