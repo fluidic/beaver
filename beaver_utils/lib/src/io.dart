c453ad2bc62ce764725fdcdb9cd52b9fcb185c18
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-11-07 10:40:05 +0900

    [beaver_utils] Add writeTextFile helper function

--- beaver_utils/lib/src/io.dart
+++ beaver_utils/lib/src/io.dart
@@ -4,3 +4,7 @@ import 'dart:io';
 /// Reads the contents of the text file [file].
 String readTextFile(String file) =>
     new File(file).readAsStringSync(encoding: UTF8);
+
+/// Writes the [contents] to the text file [file].
+void writeTextFile(String file, String contents) =>
+    new File(file).writeAsStringSync(contents, encoding: UTF8);
