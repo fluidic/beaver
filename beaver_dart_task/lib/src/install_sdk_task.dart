6b3dab651281c1fee3aed5542218d09823afe184
Kyusun Kim <maniagoon@gmail.com>
2016-11-08 13:48:00 +0900

    [dart_task] Change the behavior of InstallDartSdkTask
    It download the binary of SDK and unzip it.

--- beaver_dart_task/lib/src/install_sdk_task.dart
+++ beaver_dart_task/lib/src/install_sdk_task.dart
@@ -1,60 +1,35 @@
 import 'dart:async';
 import 'dart:io' show Platform;
 
-import 'package:args/args.dart';
 import 'package:beaver_task/beaver_task.dart';
-import 'package:quiver_iterables/iterables.dart' show concat;
 
 @TaskClass('install_dart_sdk')
 class InstallDartSdkTask extends Task {
-  final bool dev;
+  final String version;
 
-  final bool withDartium;
+  InstallDartSdkTask(this.version);
 
-  final bool withContentShell;
-
-  InstallDartSdkTask(
-      {this.dev: false, this.withDartium: false, this.withContentShell: false});
-
-  factory InstallDartSdkTask.fromArgs(List<String> args) {
-    final parser = new ArgParser(allowTrailingOptions: true)
-      ..addFlag('dev', defaultsTo: false, abbr: 'd')
-      ..addFlag('withDartium', defaultsTo: false, abbr: 'D')
-      ..addFlag('withContentShell', defaultsTo: false, abbr: 'C');
-    final results = parser.parse(args);
-    return new InstallDartSdkTask(
-        dev: results['dev'],
-        withDartium: results['withDartium'],
-        withContentShell: results['withContentShell']);
-  }
+  InstallDartSdkTask.fromArgs(List<String> args) : version = args[0];
 
   @override
   Future<Null> execute(Context context) async {
-    if (Platform.isMacOS) {
-      List<String> installOptions = [];
-      if (dev) {
-        installOptions.add('--devel');
-      }
-      if (withDartium) {
-        installOptions.add('--with-dartium');
-      }
-      if (withContentShell) {
-        installOptions.add('--with-content-shell');
-      }
-      final tasks = [
-        new ShellTask('brew', ['tap', 'dart-lang/dart']),
-        new ShellTask(
-            'brew',
-            (concat([
-              ['install', 'dart'],
-              installOptions
-            ]) as List<String>)
-                .toList())
-      ];
-      await Future.forEach(tasks, (task) => task.execute(context));
-    } else {
-      // FIXME: Support Linux and Windows
-      throw new UnsupportedPlatformException();
+    var stableOrDev = 'stable';
+    if (version.contains('dev')) {
+      stableOrDev = 'dev';
     }
+
+    var platform = 'macos';
+    if (Platform.isLinux) {
+      platform = 'linux';
+    } else if (Platform.isWindows) {
+      platform = 'windows';
+    }
+
+    final sdk =
+        'https://storage.googleapis.com/dart-archive/channels/$stableOrDev/release/$version/sdk/dartsdk-$platform-x64-release.zip';
+    await seq([
+      new DownloadTask(sdk, '.'),
+      new UnzipTask('dartsdk-$platform-x64-release.zip', '.')
+    ]).execute(context);
   }
 }
