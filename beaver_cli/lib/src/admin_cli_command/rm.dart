bb3e49433d1e6db75ee45715d73031ea1c978c69
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-11-08 13:08:46 +0900

    Implement beaver_admin rm command

--- beaver_cli/lib/src/admin_cli_command/rm.dart
+++ beaver_cli/lib/src/admin_cli_command/rm.dart
@@ -1,6 +1,14 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:args/command_runner.dart';
+import 'package:beaver_utils/beaver_utils.dart';
+import 'package:command_wrapper/command_wrapper.dart';
+import 'package:yaml/yaml.dart';
+
+import '../exit_codes.dart';
+
+final gcloudCli = new CommandWrapper('gcloud');
 
 class RmCommand extends Command {
   @override
@@ -9,10 +17,43 @@ class RmCommand extends Command {
   @override
   String get name => 'rm';
 
-  RmCommand() : super();
+  Future<Null> deleteFunction(String siteId) async {
+    final functionName = 'beaver-functions-$siteId';
+    await gcloudCli.run(['alpha', 'functions', 'delete', functionName]);
+  }
 
   @override
   Future<Null> run() async {
-    return null;
+    if (argResults.rest.length != 1) {
+      print('Specify site id to describe.');
+      printUsage();
+      exit(exitCodeError);
+    }
+    final siteId = argResults.rest[0];
+
+    await createFileIfNotExist(beaverAdminConfigPath);
+    String contents = await readTextFile(beaverAdminConfigPath);
+    Map config = deepCopy(loadYaml(contents) ?? {});
+    if (config['sites'] == null) {
+      print('$siteId does not exist');
+      exit(exitCodeError);
+    }
+    var foundSite;
+    for (final site in config['sites']) {
+      if (site['site_id'] == siteId) {
+        config['sites'].remove(site);
+        foundSite = site;
+        break;
+      }
+    }
+    if (foundSite == null) {
+      print('$siteId does not exist');
+      exit(exitCodeError);
+    }
+    config['sites'].remove(foundSite);
+    await deleteFunction(siteId);
+    await writeTextFile(beaverAdminConfigPath, toYamlString(config));
+    print('Deleted $siteId');
   }
 }
+
