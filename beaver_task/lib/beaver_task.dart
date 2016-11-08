99d35c3ac88ed24cc930098e0a0dac297c01bb0d
Kyusun Kim <maniagoon@gmail.com>
2016-11-08 13:45:16 +0900

    [task] Export archive_task.dart

--- beaver_task/lib/beaver_task.dart
+++ beaver_task/lib/beaver_task.dart
@@ -5,6 +5,7 @@ export 'src/base.dart';
 export 'src/exception.dart';
 export 'src/gcloud_context.dart';
 export 'src/task.dart';
+export 'src/task/archive_task.dart';
 export 'src/task/copy_task.dart';
 export 'src/task/delete_task.dart';
 export 'src/task/download_task.dart';
