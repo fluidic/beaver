eadc7a04dfaecab153a8c8c0c976b05bb28611bc
Kyusun Kim <maniagoon@gmail.com>
2016-11-15 16:15:18 +0900

    [trigger_handler] Make CloudInfo can parse GAE url

--- beaver_trigger_handler/lib/src/cloud_info.dart
+++ beaver_trigger_handler/lib/src/cloud_info.dart
@@ -12,13 +12,16 @@ class CloudInfo {
     // TODO: Support the other cloud platforms.
     var type;
     if (requestUrl.host.contains('cloudfunctions.net')) {
-      type = 'gcloud';
-    } else {
+      type = 'gcf';
+    } if (requestUrl.host.contains('appspot.com')) {
+      type = 'gae';
+    }else {
       type = 'local';
     }
 
     switch (type) {
       case 'gcloud':
+        final cloudType = 'gcloud';
         final exp = new RegExp(r'^([^-]+[-][^-]+)[-](.*)\..+\..+$');
         final match = exp.firstMatch(requestUrl.host);
         final region = match.group(1);
@@ -29,7 +32,15 @@ class CloudInfo {
             port: requestUrl.port,
             path: requestUrl.pathSegments.first);
         final siteId = requestUrl.pathSegments.first.split('-')[2];
-        return new CloudInfo._internal(url, type, region, projectName, siteId);
+        return new CloudInfo._internal(url, cloudType, region, projectName, siteId);
+      case 'gae':
+        final cloudType = 'gcloud';
+        final projectName = requestUrl.host.split('.')[0];
+        final url = new Uri(
+            scheme: requestUrl.scheme,
+            host: requestUrl.host,
+            port: requestUrl.port);
+        return new CloudInfo._internal(url, cloudType, 'us-central1', projectName, '');
       case 'local':
         final url = new Uri(
             scheme: requestUrl.scheme,
