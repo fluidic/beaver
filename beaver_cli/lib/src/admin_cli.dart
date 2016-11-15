d40cc612740c1afd0bb1d547d7d0d2dde66a44b3
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-11-15 17:48:01 +0900

    Remove beaver_ prefix from beaver_command_runner.dart

--- beaver_cli/lib/src/admin_cli.dart
+++ beaver_cli/lib/src/admin_cli.dart
@@ -4,7 +4,7 @@ import './admin_cli_command/create.dart';
 import './admin_cli_command/describe.dart';
 import './admin_cli_command/list.dart';
 import './admin_cli_command/delete.dart';
-import './beaver_command_runner.dart';
+import './command_runner.dart';
 
 CommandRunner getRunner() =>
     new BeaverCommandRunner('beaver_admin', 'Admin CLI for beaver CI.')
--- beaver_cli/lib/src/cli.dart
+++ beaver_cli/lib/src/cli.dart
@@ -1,6 +1,5 @@
 import 'package:args/command_runner.dart';
 
-import './beaver_command_runner.dart';
 import './cli_command/create.dart';
 import './cli_command/delete.dart';
 import './cli_command/describe.dart';
@@ -9,6 +8,7 @@ import './cli_command/list.dart';
 import './cli_command/setup.dart';
 import './cli_command/test.dart';
 import './cli_command/upload.dart';
+import './command_runner.dart';
 
 CommandRunner getRunner() =>
     new BeaverCommandRunner('beaver', 'CLI for beaver CI.')
