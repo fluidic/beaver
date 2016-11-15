14742011bad6c1c8b7860feef8a4059fe8feb62c
Kyusun Kim <maniagoon@gmail.com>
2016-11-15 19:48:22 +0900

    [docker_task] Add processWorkingDir to DockerTask

--- beaver_docker_task/lib/src/docker_task.dart
+++ beaver_docker_task/lib/src/docker_task.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:args/args.dart';
 import 'package:beaver_task/beaver_task.dart';
 import 'package:command_wrapper/command_wrapper.dart';
 
@@ -8,13 +9,22 @@ final docker = new CommandWrapper('docker');
 @TaskClass('docker')
 class DockerTask extends Task {
   final List<String> args;
+  final String processWorkingDir;
 
-  DockerTask(this.args);
-  DockerTask.fromArgs(this.args);
+  DockerTask(this.args, {String processWorkingDir})
+      : this.processWorkingDir = processWorkingDir;
+
+  factory DockerTask.fromArgs(List<String> args) {
+    final parser = new ArgParser(allowTrailingOptions: true)
+      ..addOption('process-working-dir', abbr: 'C');
+    final results = parser.parse(args);
+    return new DockerTask(results.rest,
+        processWorkingDir: results['process-working-dir']);
+  }
 
   @override
   Future<Null> execute(Context context) async {
-    final result = await docker.run(args);
+    final result = await docker.run(args, processWorkingDir: processWorkingDir);
     for (final line in result.stdout) {
       context.logger.info(line);
     }
