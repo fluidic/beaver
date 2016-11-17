48a2f2ebf973340e85dcf2a0f7579e45d63a40ae
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-11-17 12:03:01 +0900

    Load task packages when running the task server

--- beaver_task/bin/beaver_task_server.dart
+++ beaver_task/bin/beaver_task_server.dart
@@ -1,5 +1,13 @@
 import 'package:beaver_task/beaver_task_server.dart' as server;
 
+// FIXME: We need a way to specify these dependencies programmatically.
+/// [PubTask] import.
+import 'package:beaver_dart_task/beaver_dart_task.dart';
+/// [DockerTask] import
+import 'package:beaver_docker_task/beaver_docker_task.dart';
+/// [GCloudStorageUploadTask] import.
+import 'package:beaver_gcloud_task/beaver_gcloud_task.dart';
+
 void main() {
   server.serve();
 }
